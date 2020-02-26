33335e4733b5743afe7e30a6a6096135fe50980b
Badri <badri.py@outlook.com>
2020-02-26 08:06:39 +0530

    Mongoose setup in server.js with db url config

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoUri:
+    "mongodb://badri:12td0426@ds351987.mlab.com:51987/heroku_j9p9g5d6"
+};
--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+// DB config
+const db = require("./config/keys").mongoUri;
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => `Server is running on ${port}`);
+app.listen(port, () => console.log(`Server is running on ${port}`));
