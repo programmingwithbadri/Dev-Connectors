58a2ad6d995eb8a89819d1c134d434ee58a91718
Badri <badri.py@outlook.com>
2020-02-26 08:21:15 +0530

    Initialize express server with route files

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profile");
+
 const app = express();
 
 // DB config
 const db = require("./config/keys").mongoUri;
 
+// Connect to Mongoose
 mongoose
   .connect(db, {
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useUnifiedTopology: true
   })
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
+
+// Use Routes by router middleware
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
