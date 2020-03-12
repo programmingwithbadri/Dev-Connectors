f85578a17c4e385bfb015f571a371e39e50ec48f
Badri <badri.py@outlook.com>
2020-03-12 19:07:37 +0530

    User registration route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 const router = express.Router();
+
+// Load User model
+const User = require("../../models/User");
 
 // @route  GET api/users/test
 // @desc   Tests Users route
@@ -10,5 +15,43 @@ router.get("/test", (req, res) =>
     message: "Users works"
   })
 );
+
+// @route  Post api/users/register
+// @desc   Register user
+// @access Public
+router.post("/register", (req, res) => {
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
+    if (user) {
+      return res.status(400).json({
+        email: "Email already exists"
+      });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: 200, // size
+        r: "pg", // rating
+        d: "mm" // default
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        avatar
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
