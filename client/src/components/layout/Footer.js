1789b8c7b99c1e471eeb59e3b19f08dd65300e2b
Badri <badri.py@outlook.com>
2020-03-31 16:11:21 +0530

    react router setup with links

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -4,6 +4,6 @@ export default () => {
   return (
     <footer className="bg-dark text-white mt-5 p-4 text-center">
       Copyright &copy; {new Date().getFullYear()} DevConnector
-    </footer>
+    </footer> 
   );
 };
