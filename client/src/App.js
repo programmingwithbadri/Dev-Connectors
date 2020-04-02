eb62b0dc02d36e27317fb2325a4dd5ee31429b43
Badri <badri.py@outlook.com>
2020-04-02 13:36:07 +0530

    Get current profile action and reducer

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,10 @@ import Landing from "./components/layout/Landing";
 import "./App.css";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Dashboard from "./components/dashboard";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
 
 // Check for auth token
 if (localStorage.jwtToken) {
@@ -30,6 +32,9 @@ if (localStorage.jwtToken) {
     // Logout user
     store.dispatch(logoutUser());
 
+    // Clear current profile
+    store.dispatch(clearCurrentProfile());
+
     // Redirect to login
     window.location.href = "/login";
   }
@@ -46,6 +51,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
