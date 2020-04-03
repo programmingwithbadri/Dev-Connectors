6f0816fe2f5295e866b959a60f22b943f5c9fef9
Badri <badri.py@outlook.com>
2020-04-03 19:04:10 +0530

    Add experience functionality

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import { clearCurrentProfile } from "./actions/profileActions";
 import PrivateRoute from "./components/common/PrivateRoute";
 import CreateProfile from "./components/profile/CreateProfile";
 import EditProfile from "./components/profile/EditProfile";
+import AddExperience from "./components/profile/AddExperience";
 
 // Check for auth token
 if (localStorage.jwtToken) {
@@ -69,6 +70,13 @@ class App extends Component {
                   exact
                   path="/edit-profile"
                   component={EditProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
                 />
               </Switch>
             </div>
--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -56,9 +56,9 @@ export const clearCurrentProfile = () => {
 
 // Delete account & profile
 export const deleteAccount = () => dispatch => {
-  if (window.confirm('Are you sure? This can NOT be undone!')) {
+  if (window.confirm("Are you sure? This can NOT be undone!")) {
     axios
-      .delete('/api/profile')
+      .delete("/api/profile")
       .then(res =>
         dispatch({
           type: SET_CURRENT_USER,
@@ -72,4 +72,30 @@ export const deleteAccount = () => dispatch => {
         })
       );
   }
+};
+
+// Add experience
+export const addExperience = (expData, history) => dispatch => {
+  axios
+    .post("/api/profile/experience", expData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Add education
+export const addEducation = (eduData, history) => dispatch => {
+  axios
+    .post("/api/profile/education", eduData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
 };
