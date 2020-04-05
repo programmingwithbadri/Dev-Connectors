b1346de36050f2759905abd90a3d76a0d999c21f
Badri <badri.py@outlook.com>
2020-04-05 19:14:17 +0530

    Add post reducer and action

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,22 @@
+import {
+  ADD_POST,
+  POST_LOADING,
+} from "../actions/types";
+
+const initialState = {
+  posts: [],
+  post: {},
+  loading: false,
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [action.payload, ...state.posts],
+      };
+    default:
+      return state;
+  }
+}
